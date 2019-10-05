e1e4b07c7610cb4483495ebedd01c3c40528eeb2
Olha <olja.podoprigora@gmail.com>
2019-10-05 17:41:32 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -35,4 +35,70 @@ $(document).ready(function(){
 	}
 
 	}, 1000);
+
+	let options = [
+		{
+			endpoint	: 'https://reqres.in/api/users',
+			count 		: 6,
+			target		: 'competitorsUsers',
+			class		: ''
+		},
+		{
+			endpoint	: 'https://reqres.in/api/users?page=2',
+			count 		: 3,
+			target		: 'usersJury',
+			class		: 'user__img-box--round'
+		}
+		];
+	options.forEach(function(option){
+		loadUsers(option);
+	})
+
+	function loadUsers(opt){
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', opt.endpoint);
+		xhr.send();
+
+		xhr.responseType = 'json';
+
+		xhr.onerror = function() { 
+		  alert(`Ошибка соединения`);
+		};
+		xhr.onload = function() {
+		  if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+		    alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
+		  } else { // если всё прошло гладко, выводим результат
+			
+			let target = document.getElementById(opt.target),
+			users = xhr.response.data.splice(0, opt.count);
+		  	
+		  	users.forEach(function(user){
+		  		console.log(user.first_name, user.last_name);
+		  		let html = `<div class="user">
+						<div class="user__img-box ${opt.class}">
+							<img src="${user.avatar}" alt="${user.first_name} ${user.last_name}" class="user__img">
+						</div>
+						<div class="user__name">${user.first_name} ${user.last_name}</div>
+						<div class="user__pos">${user.email}</div>
+					</div>`;
+				
+				target.innerHTML = target.innerHTML + html;
+		  	});
+		  }
+		};
+
+		console.log(xhr);
+	};
+
+	let sound = document.querySelector('audio');
+
+	$('.toggler').on('click', function(e){
+		e.preventDefault();
+
+		$('body').toggleClass('menu-opened');
+		sound.play();
+	})
+
+
+	
 });
